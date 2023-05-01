a71329b1a9d95aeca7be6f238b6b97e748f8d59f
imranahmed20 <108649863+imranahmed20@users.noreply.github.com>
2023-05-01 07:37:01 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Main from './Component/Layout/Main.jsx';
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+  },
+]);
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}></RouterProvider>
+  </React.StrictMode>,
+)
